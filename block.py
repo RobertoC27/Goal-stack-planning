04dc4dc81425cb994b8eb22e8e80d92dfabe7217
RobertoC27 <chi13027@uvg.edu.gt>
2017-04-17 15:04:43 -0600

    working version

---
+++ block.py
@@ -0,0 +1,22 @@
+"""
+el objeto bloque que contiene los atributos
+y demas cosas necesarias
+"""
+
+class bloque(object):
+    def __init__(self, name, prop={}):
+        self.name = name
+        self.props = prop
+
+    def add_prop(self, name, value=False):
+        self.props[name] = value
+
+    def check_prop(self, name):
+        if name in self.props:
+            return self.props[name]
+        else:
+            return False
+
+    def set_prop(self, name, value):
+        self.props[name] = value
+
